e1bd89e7051f061ecc1c7f188b14c72f660f4e92
JuanDied <juanrayadi2014@gmail.com>
2022-05-17 13:24:30 -0500

    aniamciones
    animaciones de salto

--- MMM_Game/Assets/Scripts/Player_selector.cs
+++ MMM_Game/Assets/Scripts/Player_selector.cs
@@ -8,14 +8,18 @@ using UnityEngine.SceneManagement;
 public class Player_selector : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject player_name;
-    private TextMeshProUGUI player_name_text;
+    // [SerializeField] private GameObject player_name;
+    private GameObject player_obj;
+    private Animator animator;
+    [SerializeField]private TextMeshProUGUI player_name_text;
     [SerializeField]private TextMeshProUGUI Selected_player;
     public static string player;
    
     void Start()
     {
-        player_name_text = player_name.GetComponent<TextMeshProUGUI>();
+        // player_name_text = player_name.GetComponent<TextMeshProUGUI>();
+        animator = GetComponent<Animator>();
+        player_obj = GetComponent<GameObject>();
     }
 
     // // Update is called once per frame
@@ -23,13 +27,35 @@ public class Player_selector : MonoBehaviour
     // {
         
     // }
-    public void IwasClicked(){
-        Debug.Log("Clickeeeeedd! :  "+player_name_text.text);
-        Selected_player.text = player_name_text.text;
-    
-        player = player_name_text.text;
+    public void Next_scene(){
+        Debug.Log("continuamos");
         SceneManager.LoadScene("Personalization");
-      
-     
     }
+
+    public void IwasClicked(){
+        Debug.Log("Clickeeee2edd! :  "+player_name_text.text);
+        animator.SetTrigger("saltar");
+        Selected_player.text = player_name_text.text;
+        player = player_name_text.text;
+
+        // while((animator.GetCurrentAnimatorStateInfo(0).normalizedTime ) < 0.99f)
+        // {
+        //     Debug.Log("Jumping");
+            
+         
+
+        // }
+      // SceneManager.LoadScene("Personalization");  
+    }
+
+    public void Update(){
+        // while((animator.GetCurrentAnimatorStateInfo(0).normalizedTime ) < 0.99f)
+        // {
+        //     Debug.Log("Jumping");
+            
+         
+
+        // }
+    }
+    
 }
